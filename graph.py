4127e7bd455539a43be611ac7e70461c6cbcaf81
alma <alma.dzaferovic996@gmail.com>
2023-04-04 21:02:26 +0200

    Add centrality measures methods

--- graph.py
+++ graph.py
@@ -33,7 +33,7 @@ class Graph:
         self.adjacency_matrix = self._adjacency_matrix()
         self.adjacency_list = self._adjacency_list()
         self.adjacency_list_indexes = self._adjacency_list_indexes()
-        self.links_by_node_indexes = {}  
+        self.links_by_node_indexes = {}
         for link in self.links:
             source_index = self.node_index(link.source)
             target_index = self.node_index(link.target)
@@ -77,7 +77,7 @@ class Graph:
             neighbours[i].append(link.target)
             neighbours[j].append(link.source)
         return neighbours
-    
+
     def _adjacency_list_indexes(self):
         neighbours = [[] for i in range(self.n)]
         for k, link in enumerate(self.links):
@@ -113,7 +113,7 @@ class Graph:
                     ith_vertex_degree += 1
             vertex_degrees.append(ith_vertex_degree)
         return vertex_degrees
-    
+
     def is_connected(self):
         visited = set()
         to_visit = set()
@@ -124,8 +124,8 @@ class Graph:
             for neighbour in self.adjacency_list_indexes[current_idx]:
                 if neighbour not in visited:
                     to_visit.add(neighbour)
-        return len(visited) == self.n    
-    
+        return len(visited) == self.n
+
     def subgraphs(self):
         node_indexes = set(i for i in range(self.n))
         subgraphs = []
@@ -147,4 +147,75 @@ class Graph:
                         links.append(self.links_by_node_indexes[(i, j)])
             subgraphs.append(Graph(nodes, links))
         return subgraphs
+
+    def eigenvector_centrality(self):
+        # use power iteration method to compute largest eigenvalue
+        eigenvector = [1/math.sqrt(self.n) for i in range(self.n)]
+
+        error_margin = 1e-2
+        while True:
+            # compute the matrix-vector product A * x
+            Ax = []
+            for i in range(self.n):
+                _sum = 0
+                for j in range(self.n):
+                    _sum += self.adjacency_matrix[i][j] * eigenvector[j]
+                Ax.append(_sum)
+
+            # compute the new eigenvector centrality vector
+            norm = math.sqrt(sum(x ** 2 for x in Ax))
+            new_eigenvector = [x/norm for x in Ax]
+            # check for convergence
+            max_error = max(
+                abs(new_eigenvector[i] - eigenvector[i])
+                for i in range(self.n)
+            )
+            if max_error < error_margin:
+                break
+            eigenvector = new_eigenvector
+
+        eigenvectors_dict = {}
+        for i in range(self.n):
+            eigenvectors_dict[self.nodes[i].id] = eigenvector[i]
+        return eigenvectors_dict
     
+    # def betweenness_centrality(self):
+    #     # brandes algorithm for betweenness centrality in unweighted graphs
+    #     betweenness = [0 for i in range(self.n)]
+    #     for s in range(self.n):
+    #         print(s, "/", self.n)
+    #         S = []
+    #         P = [[] for i in range(self.n)]
+    #         sigma = [0 for i in range(self.n)]
+    #         sigma[s] = 1
+    #         d = [-1 for i in range(self.n)]
+    #         d[s] = 0
+    #         Q = collections.deque()
+    #         Q.append(s)
+    #         while len(Q) > 0:
+    #             # popleft here because Q is a queue
+    #             v = Q.popleft()
+    #             S.append(v)
+    #             for w in self.adjacency_list_indexes[v]:
+    #                 # w found first time?
+    #                 if d[w] < 0:
+    #                     Q.append(w)
+    #                     d[w] = d[v] + 1
+    #                 # shortest path to w via v?
+    #                 if d[w] == d[v] + 1:
+    #                     sigma[w] += sigma[v]
+    #                     P[w].append(v)
+    #         delta = [0 for i in range(self.n)]
+    #         # S returnes nodes in order of non-increasing distance from s
+    #         while len(S) > 0:
+    #             # pop here because S is a stack
+    #             w = S.pop()
+    #             for v in P[w]:
+    #                 delta[v] += (sigma[v] / sigma[w]) * (1 + delta[w])
+    #             if w != s:
+    #                 betweenness[w] += delta[w]
+                    
+    #     betweenness_dict = {}
+    #     for i in range(self.n):
+    #         betweenness_dict[self.nodes[i].id] = betweenness[i]
+    #     return betweenness_dict
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -3,10 +3,12 @@ import json
 import pytest
 import networkx as nx
 
+
 @pytest.fixture
 def graph():
     G = Graph.load('challenge_graph.json')
     return G
+
 
 @pytest.fixture
 def graph_nx():
@@ -18,30 +20,48 @@ def graph_nx():
     for edge in data['links']:
         G_nx.add_edge(edge['source'], edge['target'])
     return G_nx
-    
-# 1:
+
+
 def test_vertex_and_edge_counts(graph, graph_nx):
-    assert graph.vertex_count() == graph_nx.number_of_nodes() and graph.edge_count() == graph_nx.number_of_edges() 
-    
-# 2:
+    assert graph.vertex_count() == graph_nx.number_of_nodes(
+    ) and graph.edge_count() == graph_nx.number_of_edges()
+
+
 def test_vertex_degrees(graph, graph_nx):
     actual_vertex_degrees = graph.vertex_degrees()
     vd_nx_dict = dict(graph_nx.degree())
     expected_vertex_degrees = list(vd_nx_dict.values())
     assert actual_vertex_degrees == expected_vertex_degrees
 
-# 3:
+
 def test_is_connected(graph, graph_nx):
     assert nx.is_connected(graph_nx) == graph.is_connected()
-    
-# 4:
+
+
 def test_subgraphs(graph, graph_nx):
-    actual_subgraphs = set(frozenset(node.id for node in graph.nodes) for graph in graph.subgraphs())
-    expected_subgraphs = set(frozenset(graph) for graph in nx.connected_components(graph_nx))
+    actual_subgraphs = set(frozenset(node.id for node in graph.nodes)
+                           for graph in graph.subgraphs())
+    expected_subgraphs = set(frozenset(graph)
+                             for graph in nx.connected_components(graph_nx))
     assert actual_subgraphs == expected_subgraphs
-    
-# 5: 
+
+
 def test_find_isolates(graph, graph_nx):
     actual_isolates = graph.find_isolates()
     expected_isolates = list(nx.isolates(graph_nx))
     assert actual_isolates == expected_isolates
+
+
+def test_eigenvector_centrality(graph, graph_nx):
+    actual_centrality = graph.eigenvector_centrality()
+    expected_centrality = nx.eigenvector_centrality(graph_nx)
+    for id, value in actual_centrality.items():
+        expected_value = expected_centrality[id]
+        err = abs(expected_value-value)/(expected_value+value)
+        assert err < 1
+
+
+# def test_betweenness_centrality(graph, graph_nx):
+#     actual_centrality = graph.betweenness_centrality()
+#     expected_centrality = nx.betweenness_centrality(graph_nx)
+#     assert actual_centrality == expected_centrality
