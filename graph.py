414a72888968b4b79beaa1d785bfa3808b18a0a7
alma <alma.dzaferovic996@gmail.com>
2023-04-04 20:38:26 +0200

    Add more metrics and tests

--- graph.py
+++ graph.py
@@ -1,13 +1,18 @@
 import json
+import math
+import collections
+
 
 class Node:
     def __init__(self, id, q1, q2):
         self.id = id
         self.q1 = q1
         self.q2 = q2
+
     def print_node_info(self):
         print("(" + str(self.q1) + ", " + str(self.q2) + ")")
         print("Id: ", self.id)
+
 
 class Link:
     def __init__(self, id, source, target):
@@ -15,13 +20,26 @@ class Link:
         self.source = source
         self.target = target
 
+
 class Graph:
     graph_id = 0
+
     def __init__(self, nodes, links):
         Graph.graph_id += 1
         self.id = Graph.graph_id
+        self.n = len(nodes)
         self.nodes = nodes
         self.links = links
+        self.adjacency_matrix = self._adjacency_matrix()
+        self.adjacency_list = self._adjacency_list()
+        self.adjacency_list_indexes = self._adjacency_list_indexes()
+        self.links_by_node_indexes = {}  
+        for link in self.links:
+            source_index = self.node_index(link.source)
+            target_index = self.node_index(link.target)
+            self.links_by_node_indexes[(source_index, target_index)] = link
+            self.links_by_node_indexes[(target_index, source_index)] = link
+
     @classmethod
     def load(cls, filename):
         with open(filename, 'r') as f:
@@ -32,51 +50,101 @@ class Graph:
             node = Node(node_json['id'], node_json['q1'], node_json['q2'])
             nodes.append(node)
         for link_json in data["links"]:
-            link = Link(link_json['id'], link_json['source'], link_json['target'])
+            link = Link(link_json['id'],
+                        link_json['source'], link_json['target'])
             links.append(link)
         return Graph(nodes, links)
+
     def node_index(self, id):
         for i, node_element in enumerate(self.nodes):
             if id == node_element.id:
                 return i
-    def adjacency_matrix(self):
-        n = self.nodes.__len__()
-        adj_matrix = [[0 for i in range(n)] for j in range(n)]
+
+    def _adjacency_matrix(self):
+        adj_matrix = [[0 for i in range(self.n)] for j in range(self.n)]
         for k, link in enumerate(self.links):
             i = self.node_index(link.source)
             j = self.node_index(link.target)
             adj_matrix[i][j] = 1
             adj_matrix[j][i] = 1
         return adj_matrix
-    def vetrex_degrees(self):
-        vertex_degrees = 0
-        adj_matrix = self.adjacency_matrix
-        for i in range(len(adj_matrix)):
-            ith_vertex_degree = 0
-            for j in range(len(adj_matrix)):
-                if adj_matrix[i][j] == 1 or adj_matrix[j][i] == 1:
-                    c += 1
-            vertex_degrees.append(ith_vertex_degree)
-        return vertex_degrees
-    # + show isolates
-    def count_isolates(self):
-        isolates_indices = []
-        adj_matrix = self.adjacency_matrix
-        for i, node in enumerate(self.nodes):
-            count_ones = adj_matrix[i].count(1)
-            if count_ones == 0:
-                isolates_indices.append(i)
-        return isolates_indices.count(0) 
-    def vertex_count(self):
-        return len(self.nodes)
-    def edge_count(self):
-        return len(self.links)
-    def adjacency_list(self):
-        n = self.nodes.__len__()
-        neighbours = [[] for i in range(n)]
+
+    def _adjacency_list(self):
+        neighbours = [[] for i in range(self.n)]
         for k, link in enumerate(self.links):
-            i = G.node_index(link.source)
-            j = G.node_index(link.target)
+            i = self.node_index(link.source)
+            j = self.node_index(link.target)
             neighbours[i].append(link.target)
             neighbours[j].append(link.source)
         return neighbours
+    
+    def _adjacency_list_indexes(self):
+        neighbours = [[] for i in range(self.n)]
+        for k, link in enumerate(self.links):
+            i = self.node_index(link.source)
+            j = self.node_index(link.target)
+            neighbours[i].append(j)
+            neighbours[j].append(i)
+        return neighbours
+
+    def find_isolates(self):
+        isolates_indices = []
+        isolates = []
+        for i, node in enumerate(self.nodes):
+            count_ones = self.adjacency_matrix[i].count(1)
+            if count_ones == 0:
+                isolates.append(node)
+                isolates_indices.append(i)
+        return isolates
+    # + show isolates
+
+    def vertex_count(self):
+        return len(self.nodes)
+
+    def edge_count(self):
+        return len(self.links)
+
+    def vertex_degrees(self):
+        vertex_degrees = []
+        for i in range(len(self.adjacency_matrix)):
+            ith_vertex_degree = 0
+            for j in range(len(self.adjacency_matrix)):
+                if self.adjacency_matrix[i][j] == 1 or self.adjacency_matrix[j][i] == 1:
+                    ith_vertex_degree += 1
+            vertex_degrees.append(ith_vertex_degree)
+        return vertex_degrees
+    
+    def is_connected(self):
+        visited = set()
+        to_visit = set()
+        to_visit.add(0)
+        while to_visit:
+            current_idx = to_visit.pop()
+            visited.add(current_idx)
+            for neighbour in self.adjacency_list_indexes[current_idx]:
+                if neighbour not in visited:
+                    to_visit.add(neighbour)
+        return len(visited) == self.n    
+    
+    def subgraphs(self):
+        node_indexes = set(i for i in range(self.n))
+        subgraphs = []
+
+        while node_indexes:
+            i = node_indexes.pop()
+            to_visit = set([i])
+            nodes = []
+            links = []
+
+            while to_visit:
+                i = to_visit.pop()
+                nodes.append(self.nodes[i])
+
+                for j in self.adjacency_list_indexes[i]:
+                    if j in node_indexes:
+                        to_visit.add(j)
+                        node_indexes.remove(j)
+                        links.append(self.links_by_node_indexes[(i, j)])
+            subgraphs.append(Graph(nodes, links))
+        return subgraphs
+    
--- tests/test_graph.py
+++ tests/test_graph.py
@@ -25,7 +25,23 @@ def test_vertex_and_edge_counts(graph, graph_nx):
     
 # 2:
 def test_vertex_degrees(graph, graph_nx):
-    vertex_degrees = graph.vertex_degrees()
+    actual_vertex_degrees = graph.vertex_degrees()
     vd_nx_dict = dict(graph_nx.degree())
-    vertex_degrees_nx = list(vd_nx_dict.values())
-    assert vertex_degrees == vertex_degrees_nx
+    expected_vertex_degrees = list(vd_nx_dict.values())
+    assert actual_vertex_degrees == expected_vertex_degrees
+
+# 3:
+def test_is_connected(graph, graph_nx):
+    assert nx.is_connected(graph_nx) == graph.is_connected()
+    
+# 4:
+def test_subgraphs(graph, graph_nx):
+    actual_subgraphs = set(frozenset(node.id for node in graph.nodes) for graph in graph.subgraphs())
+    expected_subgraphs = set(frozenset(graph) for graph in nx.connected_components(graph_nx))
+    assert actual_subgraphs == expected_subgraphs
+    
+# 5: 
+def test_find_isolates(graph, graph_nx):
+    actual_isolates = graph.find_isolates()
+    expected_isolates = list(nx.isolates(graph_nx))
+    assert actual_isolates == expected_isolates
